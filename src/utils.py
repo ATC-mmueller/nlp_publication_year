eac627c7ba9035b59dbb95e8bcb69f9199fdd1b3
Marcus <mueller.marcus.go@gmail.com>
2022-07-25 19:43:49 +0200

    added main file, PrivateKeysHandler for API

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+class PrivateKeysHandler:
+    def __init__(self, relative_path_to_file : str):
+        from configparser import RawConfigParser
+        import os
+        self.configParser = RawConfigParser()   
+        configFilePath = os.path.join(os.path.dirname(''), relative_path_to_file) 
+        self.configParser.read(configFilePath)
+
+    def load_keys(self, section : str) -> dict:
+        return self.configParser[section]
