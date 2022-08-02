55998470ebc6609513733195e5ac5cbda7ae7537
Marcus <mueller.marcus.go@gmail.com>
2022-08-02 17:49:51 +0200

    fixed scraping function, scraped again, tested stopwords, wordclouds, lemmatization, its a mess now

--- src/scraping.py
+++ src/scraping.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_titles_years(author_url : str) -> list:
+def scrape_titles(author_url : str) -> list:
     url = 'https://www.projekt-gutenberg.org/autoren/namen/' + author_url + '.html'
     response = requests.get(url)
     if response.status_code != 200:
@@ -12,17 +12,18 @@ def scrape_titles_years(author_url : str) -> list:
         if '../../' in a['href']:
             titles_years_list.append([author_url, a.get_text(), a['href'][5:]])
     
-    for title in titles_years_list:
-        url = 'https://www.projekt-gutenberg.org' + title[2]
+def scrape_title_years(title_url : str):
+        url = 'https://www.projekt-gutenberg.org' + title_url
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
-        
         year = soup.select('meta[name = firstpub]')
-        if len(year) > 0:
-            title.append(year[0]['content'])
+        if len(year) >= 1:
+            if len(year[0]['content']) >= 1:
+                return year[0]['content'][:4]
+            else:
+                return 0
         else:
-            title.append(0)
-    return titles_years_list
+            return 0
 
 def scrape_chapters(title_url : str) -> list:
     titles_chapters_list = []
@@ -43,7 +44,7 @@ def scrape_chapters(title_url : str) -> list:
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
             # text is split into several paragraphs, that can consist of poems
-            # we staert collecting after a certain header
+            # we start collecting after the link to the page is mentioned
         paragraphs = soup.select('a[href] ~ p, .poem') 
         text = ''
         for par in paragraphs:
@@ -78,5 +79,5 @@ def scrape_death_year(author_url : str) -> int:
     categories = soup.select('#mw-normal-catlinks > ul > li')
     for cat in categories:     
         if 'deaths' in cat.get_text():
-            return int(cat.get_text()[:4])
+            return cat.get_text()[:4]
     return 0
